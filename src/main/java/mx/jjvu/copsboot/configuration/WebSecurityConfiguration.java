781538c76a0d8b01ef88e2c177ccbe1bf15d1993
Julian <zS22017046@estudiantes.uv.mx>
2025-04-20 19:05:03 -0600

    missing files

--- src/main/java/mx/jjvu/copsboot/configuration/WebSecurityConfiguration.java
+++ src/main/java/mx/jjvu/copsboot/configuration/WebSecurityConfiguration.java
@@ -4,10 +4,7 @@ import com.c4_soft.springaddons.security.oidc.starter.synchronised.resourceserve
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableMethodSecurity
